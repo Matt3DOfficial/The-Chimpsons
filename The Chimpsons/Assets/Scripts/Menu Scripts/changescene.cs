a70db9e3cd718cf5552302cab2622b1fc19048b3
Matt3DOfficial <24002898@uhi.ac.uk>
2026-02-26 15:05:28 +0000

    Fixed errors and implemented basic main menu UI with logic

--- The Chimpsons/Assets/Scripts/Menu Scripts/changescene.cs
+++ The Chimpsons/Assets/Scripts/Menu Scripts/changescene.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class UIManagement : MonoBehaviour {
+public class SceneChange : MonoBehaviour {
     public Button playButton;
     
     void Start() {
